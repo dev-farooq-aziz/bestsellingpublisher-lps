2e609c552f0d3a58deab7f4c25b29fcdca3f8331
dev-farooqaziz <dev.farooqaziz@gmail.com>
2025-03-03 02:30:10 +0500

    changes

--- src/component/LP-Two/Footer/Footer.jsx
+++ src/component/LP-Two/Footer/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
         <footer>
             <div className="bg-black py-4">
                 <div className="container">
-                    <div className="text-white flex mr-sm:flex-nowrap flex-wrap items-center gap-3 justify-between text-sm">
+                    <div className="text-white flex mr-sm:flex-nowrap flex-wrap items-center gap-3 justify-center text-sm">
                         <div className="flex mr-sm:flex-nowrap flex-wrap gap-x-2 items-center">
-                            <Link href="/terms-and-conditions" target="_blank">Terms of Use</Link>
+                            {/* <Link href="/terms-and-conditions" target="_blank">Terms of Use</Link>
                             <span className="block">|</span>
-                            <Link href="/privacy-policy" target="_blank">Privacy Policy</Link>
+                            <Link href="/privacy-policy" target="_blank">Privacy Policy</Link> */}
                         </div>
                         <span className="block text-left">© 2024 - All Rights Reserved <Link href="#" target="_blank">Bhaoo INC</Link></span>
                     </div>
